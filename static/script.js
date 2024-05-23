833b3a034adc952edfd710fe36d3e2c39b8486dd
Attahiru Kamba <bappahmuhammad18@gmail.com>
2024-05-23 15:38:24 +0100

    Update script.js

--- static/script.js
+++ static/script.js
@@ -10,7 +10,8 @@ document.addEventListener('DOMContentLoaded', () => {
     async function fetchRepositories(page = 1) {
         $('#loadingModal').modal('show');
         try {
-            const response = await fetch(`{{ url_for('repository.get_repositories') }}?page=${page}`);
+            const apiUrl = `{{ url_for('repository.get_repositories') }}`;
+            const response = await fetch(`${apiUrl}?page=${page}`);
             if (!response.ok) {
             throw new Error(`Server responded with status: ${response.status}`);
             }
@@ -79,7 +80,8 @@ document.addEventListener('DOMContentLoaded', () => {
     async function fetchAndDisplayRepoData(owner, repo) {
         $('#commitloadingModal').modal('show');
         try {
-            const commitsResponse = await fetch(`{{ url_for('repository.get_commits', owner=owner, repo=repo) }}`);
+            const apiUrl = `{{ url_for('repository.get_commits', owner=owner, repo=repo) }}`;
+            const commitsResponse = await fetch(`${apiUrl}`);
             // const branchesResponse = await fetch(`/api/${owner}/${repo}/branches`);
             // const contributorsResponse = await fetch(`/api/${owner}/${repo}/contributors`);
 
